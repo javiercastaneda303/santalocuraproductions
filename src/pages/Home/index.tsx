b94a9d64b4bc1d56d5c0bfb4f7171197d6cc1694
javiercastaneda303 <javiercastaneda303@gmail.com>
2024-02-05 02:19:25 +0100

    cambios den el inicio aunque incompletos

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,36 +5,43 @@ const Index = () => {
   return (
     <div className="home-container">
       <IconTitle title="Talleres de Teatro 2024" />
-      {/* Contenedor Flex que ajusta su dirección basado en el tamaño de pantalla */}
-      <div className="flex flex-col md:flex-row md:items-start space-y-4 md:space-y-0 md:space-x-4">
+      {/* Primer elemento: imagen y texto lado a lado */}
+      <div className="md:grid md:grid-cols-2 gap-4 items-center mb-4">
         <img
           src="/images/5.jpg"
-          className="w-full md:max-w-[400px] object-contain mx-auto"
+          className="w-full max-w-[400px] object-contain mx-auto md:mx-0"
           alt="Artístico"
         />
         <div className="text-base space-y-4">
           <p className="text-justify">
             Los talleres de “Actuación, Lúdico Escénicos y Formación Teatral ¡ACERCATE! son una
             propuesta de ocio y entretenimiento para niños, jóvenes y adultos desde 10 años
-            hasta cualquier edad y cualquier género
+            hasta cualquier edad y cualquier género.
           </p>
-
-          <img
-            src="/images/2.jpg"
-            className="w-full md:max-w-[400px] object-contain mx-auto"
-            alt="Artístico"
-          />
           <p className="text-justify">
             La práctica y el encuentro grupal contribuye a desinhibirnos y a permiten
             acercarnos a nuestra esencia creativa individual o colectiva. Promueven la lectura,
             creatividad, el buen humano y crecimiento personal mediante las herramientas tan
-            diversas que proporciona el extenso arte teatral
+            diversas que proporciona el extenso arte teatral.
           </p>
-          <h2 className="text-2xl font-bold">¿A quién va dirigido?</h2>
+        </div>
+      </div>
+
+      {/* Segundo elemento: Encabezado que ocupa todo el ancho */}
+      <h2 className="text-2xl font-bold mb-4 w-full">¿A quién va dirigido?</h2>
+
+      {/* Tercer elemento: Segunda imagen y texto lado a lado */}
+      <div className="md:grid md:grid-cols-2 gap-4 items-center">
+        <img
+          src="/images/2.jpg"
+          className="w-full max-w-[400px] object-contain mx-auto md:mx-0"
+          alt="Artístico"
+        />
+        <div className="text-base space-y-4">
           <p className="text-justify">
             Va dirigido a personas de cualquier edad. Centros escolares. Estudiantes de teatro,
             televisión. Agrupaciones culturales de asociaciones y a amantes de la lectura y
-            escritura...
+            escritura.
           </p>
           <h2 className="text-2xl font-bold">Dinámica</h2>
           <p className="text-justify">
