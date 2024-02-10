34e27fcad992fbc34be888490db717e4cab3a1f6
javiercastaneda303 <javiercastaneda303@gmail.com>
2024-02-11 00:23:21 +0100

    feat: footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -5,18 +5,20 @@ const Footer: React.FC = () => {
     <div className="footer bg-primary text-secondary">
       <div className="container flex justify-between items-center max-w-7xl mx-auto">
         <div className="contact-info">
-          <p>Correo: santamorelvalenzu@gmail.com</p>
-          <p>Teléfono: +34 672 909732</p>
-          <p>Dirección: Dirección de ejemplo</p>
-          <div>
+          <p>santalocuraproductions@gmail.com</p>
+          <p>+34 632 202 126</p>
+          {/* <div>
             <h2 className="text-secondary">Horarios:</h2>
-            <p className="text-secondary">
-              Los jueves y viernes: Una hora y media cada sesión. Reservar con antelación para
-              charlas y conferencias.
-            </p>
-          </div>
+            
+          </div> */}
         </div>
-        <div className="map w-48 h-48 bg-gray-300">Insertar mapa aquí</div>
+        {/* <div className="map w-48 h-48 bg-gray-300">Insertar mapa aquí</div> */}
+        <iframe
+          src="https://www.google.com/maps/d/u/0/embed?mid=16trxN32os9cjLq8G_1OYqugVf6N_DFw&ehbc=2E312F"
+          width="640"
+          height="480"
+          title="Cursos de Teatro"
+        ></iframe>
       </div>
     </div>
   )
